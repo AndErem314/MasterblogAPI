a80a444d4300841efdee0b5fca11b3640cc435c8
AndErem314 <andrey@Mac.fritz.box>
2025-06-13 11:43:49 +0200

    added the endpoint add_post method

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -12,7 +12,39 @@ POSTS = [
 
 @app.route('/api/posts', methods=['GET'])
 def get_posts():
+    """Show all posts"""
     return jsonify(POSTS)
+
+
+@app.route('/api/posts', methods=['POST'])
+def add_post():
+    """Handle the addition of a new blog post"""
+    data = request.get_json()
+    if not data:
+        return jsonify({"error": "No input data provided"}), 400
+
+    errors = {}
+    title = data.get("title")
+    content = data.get("content")
+
+    if not title:
+        errors["title"] = "Title is required."
+    if not content:
+        errors["content"] = "Content is required."
+    if errors:
+        return jsonify({"errors": errors}), 400
+
+
+    new_id = max((post["id"] for post in POSTS), default=0) + 1
+    new_post = {
+        "id": new_id,
+        "title": title,
+        "content": content
+    }
+
+    POSTS.append(new_post)
+    return jsonify(new_post), 201
+
 
 
 if __name__ == '__main__':
